f8ae6fdb180a662a818d8ba4f964781b7e57553f
niezicheng <niezicheng@shein.com>
2023-07-18 19:14:54 +0800

    refactor: 结构目录调整

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import removeLog from './removeLog';
+import removeLog from './editor/removeLog';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(removeLog);
